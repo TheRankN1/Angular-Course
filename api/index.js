7ef1d6019d0f92fa053744befb489da2bfa3c35a
TheRankN1 <petrutpoenaru@yahoo.com>
2022-08-29 16:50:19 +0300

    add auth design with materialize css

---
+++ api/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://localhost:27017/angular-course');
+
+app.listen(3000 , () =>{
+    console.log('Running on port 3000')
+})
