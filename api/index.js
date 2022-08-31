75f382d09d2fbc8c89e61ffbf121a2cdfa8efabc
TheRankN1 <petrutpoenaru@yahoo.com>
2022-08-31 20:46:40 +0300

    added some backend , routes , controller

--- api/index.js
+++ api/index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
 const app = express();
 
-mongoose.Promise = global.Promise;
+app.use(cookieParser())
+app.use(logger('dev'))
 
-mongoose.connect('mongodb://localhost:27017/angular-course');
+mongoose.Promise = global.Promise;
+const dbConfing = require('./config/secret')
+mongoose.connect(dbConfing.mongoUrl);
+
+const auth = require('./routes/authRoutes')
+
+app.use('/api' , auth)
 
 app.listen(3000 , () =>{
     console.log('Running on port 3000')
